4ab9d25a7f15bd642260486fabf36d083327fd39
Adriatik Dushica <adriatik.dushica@gmail.com>
2020-03-28 20:29:25 +0100

    add store configuration for chrome extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore, combineReducers } from "redux";
 import jwt from "./reducers/jwt";
 
-const store = createStore(combineReducers({ jwt }));
+const store = createStore(
+  combineReducers({ jwt }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
