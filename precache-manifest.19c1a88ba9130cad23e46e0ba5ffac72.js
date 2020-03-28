c489c3022822fed54b9a63aaf8f4ff091083ab48
Adriatik Dushica <adriatik.dushica@gmail.com>
2020-03-28 12:30:06 +0100

    Updates

---
+++ precache-manifest.19c1a88ba9130cad23e46e0ba5ffac72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3e9841a7331d7630436013b42ac4e1",
+    "url": "/hack-zurich-codevscovid-frontend/index.html"
+  },
+  {
+    "revision": "d510a2bacd7a51132fc7",
+    "url": "/hack-zurich-codevscovid-frontend/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "2982dae9b864ec2b12d2",
+    "url": "/hack-zurich-codevscovid-frontend/static/js/2.786450a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hack-zurich-codevscovid-frontend/static/js/2.786450a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d510a2bacd7a51132fc7",
+    "url": "/hack-zurich-codevscovid-frontend/static/js/main.5808b42d.chunk.js"
+  },
+  {
+    "revision": "26f004e054434790ae34",
+    "url": "/hack-zurich-codevscovid-frontend/static/js/runtime-main.0cb60042.js"
+  }
+]);
