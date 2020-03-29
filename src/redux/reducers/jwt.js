ccce136089875c8ec082085a4e8d9091b898adc3
Adriatik Dushica <adriatik.dushica@gmail.com>
2020-03-29 15:24:45 +0200

    save store to localStorage onChange

--- src/redux/reducers/jwt.js
+++ src/redux/reducers/jwt.js
@@ -1,9 +1,6 @@
 import actions from "./actions";
 
-function jwt(
-  state = { token: "asd", email: "adriatik.dushica@gmail.com" },
-  action
-) {
+function jwt(state = { token: "", email: "" }, action) {
   switch (action.type) {
     case actions.SET_JWT_TOKEN:
       return action.payload;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,37 @@
 import { createStore, combineReducers } from "redux";
 import jwt from "./reducers/jwt";
 
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch {
+    // ignore write errors
+  }
+};
+
+const persistedState = loadState();
+
 const store = createStore(
   combineReducers({ jwt }),
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
